6880568955ddc347017825a769fc65090c9473d4
bruschen <imchengr@gmail.com>
2017-07-01 17:49:41 +0800

    新增一個 BudgetController.Add() 的整合測試案例 for updated

---
+++ GOOS_SampleTests/steps/common/InsertBudgetsTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_SampleTests.steps.common
+{
+    using GOOS_SampleTests.DataModelsForIntegrationTest;
+
+    using TechTalk.SpecFlow;
+    using TechTalk.SpecFlow.Assist;
+
+    [Binding]
+    public sealed class InsertBudgetsTable
+    {
+        [Given(@"Budget table existed budgets")]
+        public void GivenBudgetTableExistedBudgets(Table table)
+        {
+            //same with BudgetCreateSteps
+            var budgets = table.CreateSet<Budget>();
+            using (var dbcontext = new NorthwindEntitiesForTest())
+            {
+                dbcontext.Budgets.AddRange(budgets);
+                dbcontext.SaveChanges();
+            }
+        }
+    }
+}
