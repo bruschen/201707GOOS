a6a1eade1e53ae813ed8da8452ace2f25605d16e
bruschen <imchengr@gmail.com>
2017-07-01 11:24:31 +0800

    建立實例化的需求和步驟

---
+++ GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    using FluentAutomation;
+
+    using TechTalk.SpecFlow;
+
+    internal class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(string yearMonth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBudget()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShouldDisplay(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GOOS_SampleTests/steps/BudgetCreateSteps.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.steps
+{
+    using FluentAutomation;
+
+    using GOOS_SampleTests.PageObjects;
+
+    [Binding]
+    [Scope(Feature = "BudgetCreate")]
+    public class BudgetCreateSteps: FluentTest
+    {
+        private BudgetCreatePage _budgetCreatePage;
+
+        public BudgetCreateSteps()
+        {
+            this._budgetCreatePage= new BudgetCreatePage(this);
+        }
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            //啟動瀏覽器-chrome
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        /// <summary>
+        /// given 情境的設定
+        /// Givens the go to adding budget page.
+        /// </summary>
+        [Given(@"go to adding budget page")]
+        public void GivenGoToAddingBudgetPage()
+        {
+            this._budgetCreatePage.Go();
+        }
+
+        /// <summary>
+        /// when 情境的設定
+        /// Whens the i add a buget for.
+        /// </summary>
+        /// <param name="amount">The amount.</param>
+        /// <param name="yearMonth">The year month.</param>
+        [When(@"I add a buget (.*) for ""(.*)""")]
+        public void WhenIAddABugetFor(int amount, string yearMonth)
+        {
+            this._budgetCreatePage
+                .Amount(amount)
+                .Month(yearMonth)
+                .AddBudget();
+        }
+
+        /// <summary>
+        /// 結果設定
+        /// Thens it should display.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        [Then(@"it should display ""(.*)""")]
+        public void ThenItShouldDisplay(string message)
+        {
+            this._budgetCreatePage.ShouldDisplay(message);
+        }
+    }
+
+}
