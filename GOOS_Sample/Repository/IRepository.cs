e498f691b6f3875bc7a716150b36e02ac10d217d
bruschen <imchengr@gmail.com>
2017-07-01 16:01:32 +0800

    add fail budgetsservice unit test

---
+++ GOOS_Sample/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_Sample.Repository
+{
+    public interface IRepository<T>
+    {
+        void Save(T entity);
+    }
+}
--- GOOS_Sample/Services/BudgetService.cs
+++ GOOS_Sample/Services/BudgetService.cs
@@ -7,9 +7,18 @@ namespace GOOS_Sample.Services
 {
     using GOOS_Sample.DataModels;
     using GOOS_Sample.Models.ViewModels;
+    using GOOS_Sample.Repository;
 
     public class BudgetService:IBudgetService
     {
+        private IRepository<Budgets> _budgetRepository;
+
+
+        public BudgetService(IRepository<Budgets> budgetRepository)
+        {
+            _budgetRepository = budgetRepository;
+        }
+
         public void Create(BudgetAddViewModel budgetAddViewModel)
         {
             using (var goosDemoEntities = new GoosDemoEntities())
