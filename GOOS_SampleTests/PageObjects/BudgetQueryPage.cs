7eec6d6a15a4af08006fe9d436a8d5a6f50be6e4
bruschen <imchengr@gmail.com>
2017-07-05 00:45:22 +0800

    add UI自動化測試腳本

---
+++ GOOS_SampleTests/PageObjects/BudgetQueryPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    using FluentAutomation;
+
+    public class BudgetQueryPage: PageObject<BudgetQueryPage>
+    {
+        public BudgetQueryPage(FluentTest test)
+            : base(test)
+        {
+            this.Url = $"{PageContext.Domain}/Budget/Query";
+        }
+
+        public void Query(string startDate, string endDate)
+        {
+            I.Enter(startDate).In("#startDate")
+             .Enter(endDate).In("#endDate")
+             .Click("input[type=\"submit\"]");
+        }
+
+        public void ShouldDisplayAmount(decimal amount)
+        {
+            I.Assert.Text(amount.ToString("##.00")).In("#amount");
+        }
+    }
+}
---
+++ GOOS_SampleTests/steps/BudgetQuerySteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.steps
+{
+    using FluentAutomation;
+
+    using GOOS_SampleTests.PageObjects;
+
+    [Binding]
+    public class BudgetQuerySteps: FluentTest
+    {
+        private BudgetQueryPage _budgetQueryPage;
+
+        public BudgetQuerySteps()
+        {
+            this._budgetQueryPage = new BudgetQueryPage(this);
+        }
+
+        [Given(@"go to budget query page")]
+        public void GivenGoToBudgetQueryPage()
+        {
+            this._budgetQueryPage.Go();
+        }
+
+        //[Given(@"Budget table existed budget")]
+        //public void GivenBudgetTableExistedBudget(Table table)
+        //{
+        //    ScenarioContext.Current.Pending();
+        //}
+
+        [When(@"Query from ""(.*)"" to ""(.*)""")]
+        public void WhenQueryFromTo(string startDate, string endDate)
+        {
+            this._budgetQueryPage.Query(startDate, endDate);
+        }
+
+        [Then(@"show budget (.*)")]
+        public void ThenShowBudget(Decimal amount)
+        {
+            this._budgetQueryPage.ShouldDisplayAmount(amount);
+        }
+    }
+}
