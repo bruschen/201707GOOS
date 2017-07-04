8ed1a8832a10dbcedc733aac92a49d2be9d91b54
bruschen <imchengr@gmail.com>
2017-07-05 01:12:22 +0800

    加入查詢預算prd code，並測試過

--- GOOS_Sample/Controllers/BudgetController.cs
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -38,5 +38,19 @@ namespace GOOS_Sample.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Query(BudgetQueryViewModel model)
+        {
+            model.Amount = 10000;
+            return View(model);
+        }
     }
 }
---
+++ GOOS_Sample/Models/ViewModels/BudgetQueryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GOOS_Sample.Models.ViewModels
+{
+    public class BudgetQueryViewModel
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public decimal? Amount { get; set; }
+    }
+}
